4e2b6bcb9ee0aa8420b9dd95d8dc66cf26360cf3
Pavel Gordeev <gordeevp@gmail.com>
2018-05-16 12:33:57 +0300

    SQL for DB creation

--- db/backup.sql
+++ db/backup.sql
@@ -1,31 +1,21 @@
-CREATE SCHEMA `onlinestore` DEFAULT CHARACTER SET utf8 ;
+CREATE DATABASE `onlinestore` DEFAULT CHARACTER SET utf8 ;
 
-use `onlinestore`
+use `onlinestore`;
 
-CREATE TABLE `onlinestore`.`group` (
-  `groupid` INT NOT NULL AUTO_INCREMENT,
-  `groupname` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' NULL,
-  PRIMARY KEY (`groupid`));
+CREATE TABLE `group` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `group_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' NULL,
+  PRIMARY KEY (`id`));
 
-CREATE TABLE `onlinestore`.`product` (
-  `productid` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `price` FLOAT NOT NULL,
   `description` VARCHAR(500) NOT NULL,
-  `groupid` INT NOT NULL,
-  `imglink` VARCHAR(145) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci',
-  PRIMARY KEY (`productid`));
+  `group_id` INT NOT NULL,
+  `img_link` VARCHAR(145) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci',
+  PRIMARY KEY (`id`));
 
-
-
-ALTER TABLE `product`
-CHANGE COLUMN `productid` `id` INT(11) NOT NULL AUTO_INCREMENT ,
-CHANGE COLUMN `groupid` `group_id` INT(11) NOT NULL ,
-CHANGE COLUMN `imglink` `img_link` VARCHAR(145) ;
-
-ALTER TABLE `onlinestore`.`group`
-CHANGE COLUMN `groupid` `id` INT(11) NOT NULL AUTO_INCREMENT ,
-CHANGE COLUMN `groupname` `group_name` VARCHAR(45) NULL DEFAULT NULL ;
 
 INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
 ('Яблочный', 10.5, 'Сок такой вот Яблочный', 1, 'хз_куда/');
