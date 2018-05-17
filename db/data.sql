91e59df281a62e6d625e4ff8049387e5464b7c8a
Pavel Gordeev <gordeevp@gmail.com>
2018-05-17 18:26:41 +0300

    split sql scripts on schema and filling on data

---
+++ db/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
+('Яблочный', 10.5, 'Сок такой вот Яблочный', 1, 'ссылка на изображение');
+
+INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
+('Знамя газ', 8.33, 'Минеральная вода', 3, 'ссылка на изображение');
+
+INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
+('Морковный', 12.5, 'Сок такой вот Морковный', 1, 'ссылка на изображение');
+
+INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
+('Моршин газ', 11.33, 'Минеральная вода', 3, 'ссылка на изображение');
+
+INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
+('Виноград', 10.5, 'Сок виноградный', 1, 'ссылка на изображение');
+
+INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
+('Знамя негаз', 8.33, 'Минеральная вода', 3, 'ссылка на изображение');
+
+INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
+('Свекольный', 33.5, 'Сок свекольный', 1, 'ссылка на изображение');
+
+INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
+('Боржоми', 8.33, 'Минеральная вода', 3, 'ссылка на изображение');
--- db/schema.sql
+++ db/schema.sql
@@ -15,10 +15,3 @@ CREATE TABLE `product` (
   `group_id` INT NOT NULL,
   `img_link` VARCHAR(145) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci',
   PRIMARY KEY (`id`));
-
-
-INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
-('Яблочный', 10.5, 'Сок такой вот Яблочный', 1, 'хз_куда/');
-
-INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
-('Знамя газ', 8.33, 'Минеральная вода', 3, 'тоже не туда');
