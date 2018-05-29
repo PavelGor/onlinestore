59f0ea096aabe535659e967de23c2ce729b9641c
Pavel Gordeev <gordeevp@gmail.com>
2018-05-29 17:27:42 +0300

    Procfile for heroku

--- db/data.sql
+++ db/data.sql
@@ -1,26 +1,26 @@
-INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
-('Яблочный', 10.5, 'Сок такой вот Яблочный', 1, 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
+INSERT INTO `product` (`name`, `price`, `description`, `img_link`) VALUES
+('Яблочный', 10.5, 'Сок такой вот Яблочный', 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
 
-INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
-('Знамя газ', 8.33, 'Минеральная вода', 3, 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
+INSERT INTO `product` (`name`, `price`, `description`, `img_link`) VALUES
+('Знамя газ', 8.33, 'Минеральная вода', 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
 
-INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
-('Морковный', 12.5, 'Сок такой вот Морковный', 1, 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
+INSERT INTO `product` (`name`, `price`, `description`, `img_link`) VALUES
+('Морковный', 12.5, 'Сок такой вот Морковный', 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
 
-INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
-('Моршин газ', 11.33, 'Минеральная вода', 3, 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
+INSERT INTO `product` (`name`, `price`, `description`, `img_link`) VALUES
+('Моршин газ', 11.33, 'Минеральная вода', 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
 
-INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
-('Виноград', 10.5, 'Сок виноградный', 1, 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
+INSERT INTO `product` (`name`, `price`, `description`, `img_link`) VALUES
+('Виноград', 10.5, 'Сок виноградный', 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
 
-INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
-('Знамя негаз', 8.33, 'Минеральная вода', 3, 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
+INSERT INTO `product` (`name`, `price`, `description`, `img_link`) VALUES
+('Знамя негаз', 8.33, 'Минеральная вода', 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
 
-INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
-('Свекольный', 33.5, 'Сок свекольный', 1, 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
+INSERT INTO `product` (`name`, `price`, `description`, `img_link`) VALUES
+('Свекольный', 33.5, 'Сок свекольный', 'https://images-na.ssl-images-amazon.com/images/I/51CvGQEnWhL.jpg');
 
-INSERT INTO `product` (`name`, `price`, `description`, `group_id`, `img_link`) VALUES
-('Боржоми', 8.33, 'Минеральная вода', 3, 'https://i.ytimg.com/vi/OorKzprjVRg/hqdefault.jpg');
+INSERT INTO `product` (`name`, `price`, `description`, `img_link`) VALUES
+('Боржоми', 8.33, 'Минеральная вода', 'https://i.ytimg.com/vi/OorKzprjVRg/hqdefault.jpg');
 
 INSERT INTO `user` (`id`, `name`, `password`, `role`) VALUES ('1', 'admin', 'admin', 'ADMIN');
 INSERT INTO `user` (`id`, `name`, `password`, `role`) VALUES ('2', 'user', 'user', 'USER');
--- db/schema.sql
+++ db/schema.sql
@@ -2,17 +2,11 @@ CREATE DATABASE `onlinestore` DEFAULT CHARACTER SET utf8 ;
 
 use `onlinestore`;
 
-CREATE TABLE `group` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `group_name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' NULL,
-  PRIMARY KEY (`id`));
-
 CREATE TABLE `product` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `price` FLOAT NOT NULL,
   `description` VARCHAR(500) NOT NULL,
-  `group_id` INT NOT NULL,
   `img_link` VARCHAR(145) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci',
   PRIMARY KEY (`id`));
 
