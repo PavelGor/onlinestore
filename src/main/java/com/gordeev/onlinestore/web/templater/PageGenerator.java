71fc0e1b77cace82806e5b53f219a1f6748d5b6b
Pavel Gordeev <gordeevp@gmail.com>
2018-05-30 23:41:29 +0300

    heroku templates2

--- src/main/java/com/gordeev/onlinestore/web/templater/PageGenerator.java
+++ src/main/java/com/gordeev/onlinestore/web/templater/PageGenerator.java
@@ -19,9 +19,9 @@ public class PageGenerator {
     }
 
     public String getPage(String filename, Map<String, Object> data) {
-        File file = new File("webapp/os-jar-with-dependencies.jar!",filename);
+        File file = new File("../webapp/templates",filename);
         Writer stream = new StringWriter();
-        try { //ddd
+        try {
             String filePath;
             filePath = getClass().getClassLoader().getResource(file.getPath().replace("\\","/")).getPath();
             filePath = filePath.substring(filePath.indexOf("target"));
