d70ccb9c034a301b58ee59e45552ed7d07adcbc9
Pavel Gordeev <gordeevp@gmail.com>
2018-06-25 22:58:14 +0300

    resolve problem with template error2

--- src/main/java/com/gordeev/onlinestore/web/templater/ThymeleafPageGenerator.java
+++ src/main/java/com/gordeev/onlinestore/web/templater/ThymeleafPageGenerator.java
@@ -7,17 +7,16 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import javax.servlet.ServletContext;
 
 public class ThymeleafPageGenerator {
-    private TemplateEngine templateEngine;
 
     public TemplateEngine getTemplateEngine(ServletContext servletContext) {
         ServletContextTemplateResolver templateResolver =
                 new ServletContextTemplateResolver(servletContext);
         templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setPrefix("webapp/templates/");
+        templateResolver.setPrefix("/templates/");
         templateResolver.setSuffix(".html");
 //        templateResolver.setCacheTTLMs(3600000L);
         //templateResolver.setCharacterEncoding("utf-8");
-        templateEngine = new TemplateEngine();
+        TemplateEngine templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
         return templateEngine;
     }
